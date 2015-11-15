c53873b10776fe85ad0a754f2f82fa158dc8d8b7
veratulips <yixuewang18@gmail.com>
2015-11-15 09:23:32 +0800

    exTodo learned debug and auto reloading

--- _src/om2py4w/exTodolist.py
+++ _src/om2py4w/exTodolist.py
@@ -1,15 +1,16 @@
 # todo-list emample 
 
-import sqlite3
-from bottle import route, run
+import sqlite3 # SQL package
+from bottle import route, run, debug
 
-@route('/todo')
-def todo_list():
+@route('/todo') # create a route named todo 
+def todo_list(): # define the todo function 
 	conn = sqlite3.connect('todo.db')
 	c = conn.cursor()
 	c.execute("SELECT id, task FROM todo WHERE status LIKE '1'")
 	result = c.fetchall()
-	return str(result)
+	return str(result) # return value would show on webpage
 
-run()
+debug(True) # for developing use
+run(reloader=True) # auto reloading
 
